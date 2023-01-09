5323b0ac77a0c5928420beed3f209beb159cda35
sumi-0011 <selina2000@naver.com>
2023-01-10 00:43:28 +0900

    feat: 뒤로가기 routing 기능 추가

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ function App({ $target }) {
 
   init(this.route);
 
+  // 뒤로가기를 했을때 렌더링이 되지 않은 문제
+  // window의 popstate 이벤트를 통해, 뒤로가기나 앞으로 가기 등으로 브라우저의 URL이 변경된 경우를 감지
+  window.addEventListener('popstate', this.route);
+
   this.route();
 }
 
