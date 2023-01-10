73def630639ffc5d6f70e90469d597dc4d76cf49
sumi-0011 <selina2000@naver.com>
2023-01-11 00:12:15 +0900

    feat: 게시글 리스트 불러오기 수정

--- src/api/post.js
+++ src/api/post.js
@@ -1,4 +1,4 @@
-import { requestGET } from '.';
+import { requestGET, requestPATCH, requestPOST, requestDELETE } from '.';
 
 const getPost = async (postId) => {
   const { data } = await requestGET(`/post/${postId}`);
@@ -6,4 +6,43 @@ const getPost = async (postId) => {
   return data;
 };
 
-export { getPost };
+const getPostList = async () => {
+  const { data } = await requestGET(`/posts`);
+
+  return data;
+};
+
+const addPost = async (title, content, image) => {
+  image = `https://img.freepik.com/premium-photo/small-tricolor-kitten-meows-floorroom_
+  457211-10960.jpg?w=1060`;
+
+  const { data } = await requestPOST(`/post`, { title, content, image });
+
+  console.log('addPost data: ', data);
+  return data;
+};
+
+const modifyPost = async (postId, title, content, image) => {
+  const body = { title, content, image };
+  cleanObj(body);
+
+  const data = await requestPATCH(`/post/${postId}`, body);
+
+  return data;
+};
+const deletePost = async (postId) => {
+  const res = await requestDELETE(`/post/${postId}`);
+  console.log('res: ', res);
+
+  return res;
+};
+
+const cleanObj = (obj) => {
+  for (var propName in obj) {
+    if (obj[propName] === null || obj[propName] === undefined) {
+      delete obj[propName];
+    }
+  }
+};
+
+export { getPost, getPostList, addPost, modifyPost, deletePost };
