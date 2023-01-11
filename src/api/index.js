008112890d08f8a0cf40b7dc5513c5c89216a6cd
sumi-0011 <selina2000@naver.com>
2023-01-11 13:08:42 +0900

    feat: 새 글 작성하기 기능 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,14 @@
 const BASE_PATH = 'http://43.201.103.199';
 
 export const requestGET = async (url, options = {}) => {
-  try {
-    const fullUrl = `${BASE_PATH}${url}`;
-    const response = await fetch(fullUrl, options);
+  const fullUrl = `${BASE_PATH}${url}`;
+  const response = await fetch(fullUrl, options);
 
-    if (response.ok) {
-      const json = await response.json();
-      return json;
-    }
-    throw new Error('API 통신 실패');
-  } catch (error) {
-    console.log('error: ', error);
+  if (response.ok) {
+    const json = await response.json();
+    return json;
   }
+  throw new Error({ name: 'API 통신 실패', message: 'API 통신 실패' });
 };
 
 export const requestPOST = async (url, body) => {
